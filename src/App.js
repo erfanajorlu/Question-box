ea58bf414e16b43351c40604737d5a92712667b8
erfan <erfanajoglu098@gmail.com>
2023-11-05 21:30:08 +0330

    add tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import React , {useState} from "react";
+import Question from './Question'
+import data from './data'
 
 function App() {
+  const [questions , setQuestions] = useState(data);
   return (
-    <div className="App">
-      
-    </div>
+    <div className="w-full h-screen bg-purple-800 flex justify-center items-center">
+      <div className="w-1/2 border bg-white h-2/3 rounded-lg flex justify-between py-10">
+        <div className="w-1/6 mx-16 ">
+          <h1 className="text-lg font-bold">Questions And Answers About Login</h1>
+        </div>
+        <div>
+
+        </div>
+      </div>
+   </div>
   );
 }
 
